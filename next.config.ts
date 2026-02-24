6d63041ff548f6746e94d67c310635e01ee7ebde
seokjunh <seokjunh97@gmail.com>
2026-02-24 16:05:39 +0900

    update

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/ko",
-        permanent: true, // 308 리다이렉트
-      },
-    ];
-  },
 };
 
 const withNextIntl = createNextIntlPlugin();
