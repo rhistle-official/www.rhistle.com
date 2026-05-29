1358403cfd751c86c05fc201aa6f199a144b6764
seokjunh <seokjunh97@gmail.com>
2026-05-29 11:26:02 +0900

    feat: CallToAction 브로셔 버튼 선택적 처리

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
 
-const CallToAction = ({ href, name }: { href: string; name: string }) => {
+const CallToAction = ({ href, name }: { href?: string; name?: string }) => {
   const t = useTranslations("home.cta");
 
   return (
@@ -9,13 +9,15 @@ const CallToAction = ({ href, name }: { href: string; name: string }) => {
       <div className="mx-auto max-w-7xl items-center justify-between space-y-2 px-8 py-10 text-sm sm:flex md:py-15 md:text-base xl:py-20 xl:text-xl">
         <p>{t("message")}</p>
         <div className="space-x-2 space-y-2">
-          <a
-            href={href}
-            download={t(`download.${name}`)}
-            className="inline-block rounded-3xl bg-rhistle px-6 py-2 text-white hover:bg-rhistle/90"
-          >
-            {t(`name.${name}`)}
-          </a>
+          {href && name && (
+            <a
+              href={href}
+              download={t(`download.${name}`)}
+              className="inline-block rounded-3xl bg-rhistle px-6 py-2 text-white hover:bg-rhistle/90"
+            >
+              {t(`name.${name}`)}
+            </a>
+          )}
           <Link
             href={"/contact"}
             className="inline-block rounded-3xl bg-rhistle px-6 py-2 text-white hover:bg-rhistle/90"
